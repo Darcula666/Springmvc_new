e6f839a74fa690b34b42b27c666d653874f84344
xiaobu <1172504039@qq.com>
2015-10-09 13:55:17 +0800

    method：get、post、delete、put

--- src/main/java/com/springapp/mvc/Controller/UserController.java
+++ src/main/java/com/springapp/mvc/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.springapp.mvc.service.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -109,6 +110,47 @@ public class UserController {
     @RequestMapping(value = "/testAntPath/*/abc")
     public String testAntPath() {
         System.out.println("testAntPath");
+        return "ok";
+    }
+    /**
+     * testPathVariable
+     */
+    @RequestMapping(value = "/testPathVariable/{id}")
+    public String testPathVariable(@PathVariable("id") Integer id) {
+        System.out.println("testPathVariable"+id);
+        return "ok";
+    }
+    /**
+     * testRest get
+     */
+    @RequestMapping(value = "/testRest/{id}" ,method = RequestMethod.GET)
+    public String testRest(@PathVariable("id") Integer id) {
+        System.out.println("testRest Get:"+id);
+        return "ok";
+    }
+    /**
+     * testRest post
+     */
+    @RequestMapping(value = "/testRest",method = RequestMethod.POST)
+    public String testRest() {
+        System.out.println("testRest Post");
+        return "ok";
+    }
+    /**
+     * testRest delete
+     */
+    @RequestMapping(value = "/testRest/{id}" ,method = RequestMethod.DELETE)
+    public String testRestDelete(@PathVariable("id") Integer id) {
+        System.out.println("testRest Delete"+id);
+        return "ok";
+    }
+    /**
+     * testRest put
+     *
+     */
+    @RequestMapping(value = "/testRest/{id}" ,method = RequestMethod.PUT)
+    public String testRestPut(@PathVariable("id") Integer id) {
+        System.out.println("testRest Put"+id);
         return "ok";
     }
 }
--- src/main/webapp/WEB-INF/pages/init.jsp
+++ src/main/webapp/WEB-INF/pages/init.jsp
@@ -11,12 +11,34 @@
     <title>init</title>
 </head>
 <body>
-<a href="/user/testAntPath/asdasdasd/abc">testAntPath</a><br>
 
+<br>
+<a href="/user/testRest/1">Test Rest Get</a>
+<br>
+
+<form action="/user/testRest" method="post">
+    <input type="submit" value="Test Rest Post">
+</form>
+<br>
+
+<form action="/user/testRest/1" method="post">
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" value="Test Rest Delete">
+</form>
+<br>
+
+<form action="/user/testRest/1" method="post">
+    <input type="hidden" name="_method" value="PUT">
+    <input type="submit" value="Test Rest Put">
+</form>
+<br>
+<a href="/user/testAntPath/asdasdasd/abc">testAntPath</a><br>
+<a href="/user/testPathVariable/87">testPathVariable</a><br>
 <a href="/user/testParamsAndHeaders?username=tt&age=20">测试参数和请求头</a><br>
- <a href="/user/testMethod">测试方法</a><br>
- <form action="/user/testMethod" method="post">
-   <input type="submit" value="sub">
- </form>
+<a href="/user/testMethod">测试方法</a><br>
+
+<form action="/user/testMethod" method="post">
+    <input type="submit" value="sub">
+</form>
 </body>
 </html>
