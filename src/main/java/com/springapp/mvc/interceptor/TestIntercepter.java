c0d72cceb144ab76802d7491d57d8c53fe5fc40c
xiaobu <1172504039@qq.com>
2016-02-02 17:02:27 +0800

    加拦截器

---
+++ src/main/java/com/springapp/mvc/interceptor/TestIntercepter.java
@@ -0,0 +1,32 @@
+package com.springapp.mvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * springmvc拦截器
+ * 可以处理一些公共问题：
+ * 1、乱码
+ * 2.权限控制
+ * ...
+ */
+public class TestIntercepter implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("执行了preHandle方法");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("执行了postHandle方法");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("执行了afterCompletion方法");
+    }
+}
